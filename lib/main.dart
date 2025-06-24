043dc448d00a86821ee6f6d2572bb79de6d206f3
golamshakib <golamshakib.h@gmail.com>
2025-06-24 18:56:30 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'app.dart';
 import 'core/services/Auth_service.dart';
 import 'core/utils/logging/loggerformain.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await AuthService.init();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
         (value) {
